281e44bd4daf7c49be699be4f3e5dad26cccc307
Arachi10 <noraarachi10@gmail.com>
2023-06-15 11:03:04 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
-* Description: main - Prints out a string to stdout.
-*
+* main - Prints out a string to stdout.
 * Return: 0 if success
 */
 
 int main(void)
-
+/* description :this is the main method*/
 {
  puts("\"Programming is like building a multilingual puzzle");
  return (0);
