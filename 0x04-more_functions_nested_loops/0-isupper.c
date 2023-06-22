2e35264933c4fe0ab8c31eccc1b688415719590a
Arachi10 <noraarachi10@gmail.com>
2023-06-22 12:13:06 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,22 @@
 #include "main.h"
+
 /**
-*_isupper - entry point
-*
-*@c: the int to print
-*
-*Return: always 0 (success)
-*/
+ *_isupper - entry point
+ *
+ *@c: the int to print
+ *
+ *Return: always 0 (success)
+ */
+
 int _isupper(int c)
 {
-	if (c > 64 && c < 91)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+if (c > 64 && c < 91)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
 }
